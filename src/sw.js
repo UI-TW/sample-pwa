32bffb473f31630685236a9c258ed606c918834a
Ramanathan <mrama@thoughtworks.com>
2018-11-21 22:15:36 +0530

    added npm run all for concurrent tasks

--- src/sw.js
+++
@@ -1,3 +0,0 @@
-importScripts('workbox-sw.prod.v2.1.2.js');
-const workboxSW = new WorkboxSW();
-workboxSW.precache([]);
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,14 +1,23 @@
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpackConfig = require('./base');
+const path = require('path');
 
 webpackConfig.entry = './src';
-webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
-  output: {
-    comments: false
-  },
-  compress: {
-    warnings: false
-  }
-}));
+webpackConfig.plugins.push(
+  new CopyWebpackPlugin([
+    {
+      from: path.resolve(__dirname, '../', 'src/images'),
+      to: path.resolve(__dirname, '../', 'dist/images')
+    }]
+  ),
+  new webpack.optimize.UglifyJsPlugin({
+    output: {
+      comments: false
+    },
+    compress: {
+      warnings: false
+    }
+  }));
 
 module.exports = webpackConfig;
